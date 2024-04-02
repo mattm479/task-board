e5f9a111181e9d7537d24dc11ec08272ed633974
Matthew Miskov <mattm479@gmail.com>
2024-04-02 09:49:06 -0400

    Added code to updates a tasks status after being dropped into a new lane

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,7 +142,18 @@ function handleDeleteTask(){
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
+    const tasks = readTasksFromStorage();
+    const taskId = ui.draggable[0].dataset.taskId;
+    const newStatus = event.target.id;
 
+    for (let task of tasks) {
+        if (task.id === taskId) {
+            task.status = newStatus;
+        }
+    }
+
+    addTasksToStorage(tasks);
+    renderTaskList();
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
