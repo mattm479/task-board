40b33185920f6a5bc1e51a2b9431348961da9c3c
Matthew Miskov <mattm479@gmail.com>
2024-04-02 09:45:24 -0400

    Added element selectors and helper functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
-// Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
 const taskFormEl = $("#task-form");
+const taskDisplayEl = $(".swim-lanes");
+const todoListEl = $("#todo-cards");
+const inProgressListEl = $("#in-progress-cards");
+const doneListEl = $("#done-cards");
+const titleEl = $("#title");
+const dueDateEl = $("#due-date");
+const descriptionEl = $("#description");
+
+function readTasksFromStorage() {
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    if (tasks === null) tasks = [];
+
+    return tasks;
+}
+
+function addTasksToStorage(tasks) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -20,30 +35,11 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-    event.preventDefault();
 
-    const title = $("#title").val();
-    const dueDate = $("#due-date").val();
-    const description = $("#description").val();
-
-    $("#formModal").modal('hide');
-
-    const task = {
-        id: generateTaskId(),
-        title: title,
-        dueDate: dueDate,
-        description: description
-    };
-
-    createTaskCard(task);
-
-    title.val("");
-    dueDate.val("");
-    description.val("");
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(){
 
 }
 
@@ -54,17 +50,5 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-    renderTaskList();
 
-    taskFormEl.on('submit', handleAddTask);
-
-    $(".lane").droppable({
-        accept: ".draggable",
-        drop: handleDrop
-    });
-
-    $("#due-date").datepicker({
-        changeMonth: true,
-        changeYear: true
-    });
 });
